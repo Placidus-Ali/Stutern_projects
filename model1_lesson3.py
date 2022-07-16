21aabf5725854c89234406e47f94ac0d1b435fa2
Placidus C. Ali <108812124+Placidus-Ali@users.noreply.github.com>
2022-07-17 00:21:30 +0100

    Add files via upload
    First commit

---
+++ model1_lesson3.py
@@ -0,0 +1,15 @@
+# Exponential Calculator
+
+print("Exponential Calculator")
+x = int(input())
+y = int(input())
+
+# give a variable name in the calculation
+num = x**y
+
+if y == 0:
+    print("x =", 1)
+
+else:
+    print("x =", x)
+
